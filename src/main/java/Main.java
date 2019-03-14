b03b326dfacff44a77117f4b0d9bdc685dc7f5a4
dev4848 <devrc13@gmail.com>
2019-03-14 15:43:23 -0400

    Dev Patel

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -2,6 +2,5 @@ public class Main {
     public static void main(String[] args) {
         System.out.println("hello");
         System.out.println("Dev");
-        System.out.println("Paul Mara");
     }
 }
